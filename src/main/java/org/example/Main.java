007d471d252149093328ce9b5c93dab2bb98dcd2
Bautista Serqueira Fucile <id640794@outlook.com>
2025-04-10 19:08:17 -0300

    Refinado del helper y seteo de base de datos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import org.example.utils.HelperMySQL;
+
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws SQLException {
+        HelperMySQL dbMySQL = new HelperMySQL();
+        dbMySQL.createTables();
     }
 }
--- src/main/java/org/example/utils/HelperMySQL.java
+++ src/main/java/org/example/utils/HelperMySQL.java
@@ -39,6 +39,10 @@ public class HelperMySQL {
     }
 
     public void dropTables() throws SQLException {
+        String dropFactura_Producto = "DROP TABLE IF EXISTS Factura_Producto";
+        this.conn.prepareStatement(dropFactura_Producto).execute();
+        this.conn.commit();
+
         String dropCliente = "DROP TABLE IF EXISTS Cliente";
         this.conn.prepareStatement(dropCliente).execute();
         this.conn.commit();
@@ -47,14 +51,48 @@ public class HelperMySQL {
         this.conn.prepareStatement(dropFactura).execute();
         this.conn.commit();
 
-        String dropFactura_Producto = "DROP TABLE IF EXISTS Factura_Producto";
-        this.conn.prepareStatement(dropFactura_Producto).execute();
-        this.conn.commit();
-
         String dropProducto = "DROP TABLE IF EXISTS Producto";
         this.conn.prepareStatement(dropProducto).execute();
         this.conn.commit();
     }
+
+    public void createTables() throws SQLException {
+        String tableCliente = "CREATE TABLE IF NOT EXISTS Cliente(" +
+                "idCliente INT NOT NULL, " +
+                "nombre VARCHAR(500), " +
+                "mail VARCHAR(150), " +
+                "CONSTRAINT Cliente_pk PRIMARY KEY (idCliente));" ;
+        this.conn.prepareStatement(tableCliente).execute();
+        this.conn.commit();
+
+        String tableFactura = "CREATE TABLE IF NOT EXISTS Factura(" +
+                "idFactura INT NOT NULL, " +
+                "idCliente INT NOT NULL, " +
+                "CONSTRAINT Factura_pk PRIMARY KEY (idFactura), " +
+                "CONSTRAINT FK_idCliente FOREIGN KEY (idCliente) REFERENCES Cliente (idCliente))";
+        this.conn.prepareStatement(tableFactura).execute();
+        this.conn.commit();
+
+        String tableProducto = "CREATE TABLE IF NOT EXISTS Producto(" +
+                "idProducto INT NOT NULL, " +
+                "nombre VARCHAR(45), " +
+                "valor FLOAT," +
+                "CONSTRAINT Producto_pk PRIMARY KEY (idProducto))";
+        this.conn.prepareStatement(tableProducto).execute();
+        this.conn.commit();
+
+        String tableFactura_Producto = "CREATE TABLE IF NOT EXISTS Factura_Producto(" +
+                "idFactura INT NOT NULL," +
+                "idProducto INT NOT NULL, " +
+                "cantidad INT NOT NULL, " +
+                "CONSTRAINT Factura_Producto_pk PRIMARY KEY (idFactura, idProducto), " +
+                "CONSTRAINT FK_Factura FOREIGN KEY (idFactura) REFERENCES Factura (idFactura), " +
+                "CONSTRAINT FK_Producto FOREIGN KEY (idProducto) REFERENCES Producto (idProducto))";
+        this.conn.prepareStatement(tableFactura_Producto).execute();
+        this.conn.commit();
+
+    }
+
 
 
 }
